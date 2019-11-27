80ff3a7e64b6cec26e2a978248d39ffb0a805f38
mirecmrozek <miroslav.mrozek@vacuumlabs.com>
2019-11-27 10:20:37 +0100

    Updates

---
+++ precache-manifest.7f689263401024c530cffb22abb42fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc3576015845a013341155be3abfdc2",
+    "url": "/react-xkcd/index.html"
+  },
+  {
+    "revision": "5b418af02edec74bc497",
+    "url": "/react-xkcd/static/css/2.1c29a7ee.chunk.css"
+  },
+  {
+    "revision": "dabc96dd01a7d5169515",
+    "url": "/react-xkcd/static/css/main.22b4b372.chunk.css"
+  },
+  {
+    "revision": "5b418af02edec74bc497",
+    "url": "/react-xkcd/static/js/2.3e4108e9.chunk.js"
+  },
+  {
+    "revision": "dabc96dd01a7d5169515",
+    "url": "/react-xkcd/static/js/main.614adf32.chunk.js"
+  },
+  {
+    "revision": "60898e579db0d4b8f162",
+    "url": "/react-xkcd/static/js/runtime-main.43a13768.js"
+  }
+]);
